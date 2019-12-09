30299a3b96532ed6853bc84d0113e2ccd25bd13e
Islam <skyart450@gmail.com>
2019-12-09 11:07:30 +0500

    register commit

---
+++ src/com/team/LMS/model/Admin.java
@@ -0,0 +1,26 @@
+package com.team.LMS.model;
+
+import java.sql.SQLException;
+
+public class Admin extends Person {
+    public Admin(String username, String password, String firstName, String lastName) throws SQLException {
+        super(username, password, firstName, lastName);
+    }
+
+    @Override
+    public boolean login(String username, String password) {
+        String query = "SELECT * from Admin;";
+        return exists(username, password, query);
+
+    }
+
+
+
+    @Override
+    public void register() {
+
+    }
+
+
+
+}

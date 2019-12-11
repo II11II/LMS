f127e6dbbd399e529f184e7b5d6d6af30505028d
Aristo Crat <aristakrat31@gmail.com>
2019-12-11 19:40:55 +0500

    User Authentication is perfect

--- src/com/team/LMS/controller/login/LoginController.java
+++ src/com/team/LMS/controller/login/LoginController.java
@@ -1,14 +1,18 @@
 package com.team.LMS.controller.login;
 
-
 import com.team.LMS.model.Librarian;
 import com.team.LMS.model.Student;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class LoginController {
@@ -21,12 +25,23 @@ public class LoginController {
     @FXML
     public Label lblErrors;
 
-    public void onClickButton() throws SQLException {
+    public void userAuthentication() throws SQLException, IOException {
         Student student = new Student();
         Librarian librarian = new Librarian();
-        if (student.login(txtUsername.getText(), txtPassword.getText())) {
-            System.out.println("Welcome student ");
-        } else if (librarian.login(txtUsername.getText(), txtPassword.getText()))
-            System.out.println("Welcome Professor !");
+        String fxml = "view/librarian/Librarian.fxml";
+        if (txtUsername.getText().length() == 8) {
+            if (student.login(txtUsername.getText(), txtPassword.getText())) {
+                System.out.println("Welcome student ");
+            }
+        } else if (librarian.login(txtUsername.getText(), txtPassword.getText())) {
+            System.out.println("librarian");
+            Stage primaryStage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("/com/team/LMS/view/librarian/LibrarianDashboard.fxml"));
+            primaryStage.setTitle("Library Management System");
+            primaryStage.setScene(new Scene(root, 800, 500));
+            primaryStage.show();
+        }
     }
+
+
 }
